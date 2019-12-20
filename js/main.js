a14ead17093f765af5690a9c3978f84e971e7c97
Lepetitdesigner <46657593+Lepetitdesigner@users.noreply.github.com>
2019-12-20 17:15:01 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+    function ToggleClass() {
+
+        var hamburger = document.getElementById("hamburger");
+        var menu = document.getElementById("menu");
+
+        hamburger.classList.toggle("is-active");
+        menu.classList.toggle("actif");
+      }
+       
